0486734da783a3d7b924d3f5e77e67582fd9c36f
Gradolph <pablo.gradolph@gmail.com>
2022-09-15 11:11:50 +0200

    Clase Práctica (15/09/2022). 1ª hora

---
+++ Multiplicacion.cpp
@@ -0,0 +1,16 @@
+//Multiplicación de números introducidos por terminal
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float num1, num2;
+    cout<<"Escribe un real: "; cin>>num1;
+
+    cout<<"Escribe otro real: "; cin>>num2;
+
+    cout<<num1<< " * " << num2<< " = " << num1*num2<<endl;
+    
+    return 0;
+    
+}
