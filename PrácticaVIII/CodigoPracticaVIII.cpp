8f3e56270aa66ad82cc88a0aaac586c93bb072de
Gradolph <pablo.gradolph@gmail.com>
2022-10-09 22:08:34 +0200

    Práctica VIII

--- PrácticaVIII/CodigoPracticaVIII.cpp
+++ PrácticaVIII/CodigoPracticaVIII.cpp
@@ -8,25 +8,29 @@ using namespace std;
 #include <cmatrix>
 using namespace techsoft;
 
+// Función que retorna True o False en función de si la matriz pasada como parámetro es Diagonal Dominante o no.
 bool DominanteDiagonal(matrix<double> A){
     bool dominante = false;
     int arows = 0, acols = 0;
     arows = A.rowno(); acols = A.colno();
-    int vectorbool[arows] = {0, 0, 0, 0}; 
+    int vectorbool[arows] = {0}; 
     
+    // Se calcula la suma por fila de los elementos que no están en la diagonal.
+    // Se compara con el elemento de la diagonal y guardamos en el vectorbool un 1 o un 0 en función de si es mayor o no.
     double sum = 0.0;
     for (int i=0; i<arows; i++){
         sum = 0.0;
         for (int j=0; j<acols; j++){
             if (i!=j){
-                sum = sum + A(i, j);
+                sum = sum + fabs(A(i, j));
             } 
         }
-        if (fabs(A(i,i)) > fabs(sum)){
+        if (fabs(A(i,i)) > sum){
             vectorbool[i] = 1;
         }
     }
 
+    // Si hay algún cero en el vector, la variable dominante es false y se retorna.
     for (int i=0; i<arows; i++){
         if (vectorbool[i] == 0){
             dominante = 0;
@@ -36,17 +40,20 @@ bool DominanteDiagonal(matrix<double> A){
 
     return dominante;
 }
+
+// Función que calcula la norma máxima de un vector. Pasando como parámetro un vector.
 double NormaMaximaVector(double vector[], int len){
     double maximo = 0.0;
     for (int i=0; i<len; i++){
-        if (vector[i]>maximo){
-            maximo = vector[i];
+        if (fabs(vector[i])>maximo){
+            maximo = fabs(vector[i]);
         }
     }
 
     return maximo;
 }
 
+// Función que calcula la nomra máxima de una matriz. Pasando como parámetro una matriz.
 double NormaMaximaMatriz(matrix<double> A){
     int arows = 0, acols = 0;
     arows = A.rowno();
@@ -67,6 +74,7 @@ double NormaMaximaMatriz(matrix<double> A){
     return maximo;
 }
 
+// Función que calcula la solución de un sistema de ecuaciones lineales mediante el método iterativo de Jacobi.
 matrix<double> MetodoJacobi(matrix<double> A, matrix<double> B){
     int iteraciones = 0;
     int arows = 0, acols = 0, brows = 0, bcols = 0;
@@ -75,22 +83,122 @@ matrix<double> MetodoJacobi(matrix<double> A, matrix<double> B){
     brows = B.rowno();
     bcols = B.colno();
 
-    matrix<double> old_x(arows, acols); old_x.null();
-    matrix<double> new_x(arows, acols); new_x.null();
+    matrix<double> old_x(brows, bcols); old_x.null();
+    matrix<double> new_x(brows, bcols); new_x.null();
+    matrix<double> new_A(arows, acols); new_A.null();
 
+    // Calculamos las matrices de coeficientes
     for (int i=0; i<arows; i++){
         B(i,0) = B(i,0)/A(i,i);
         for(int j=0; j<acols; j++){
             if(i!=j){
-                A(i,j) = A(i,j)/A(i,i);
+                new_A(i,j) = -A(i,j)/A(i,i);
             } else {
-                A(i,j) = 0;
+                new_A(i,j) = 0;
             }
-            
         }
     }
 
-    return A;
+    // Definimos la matriz solución exacta
+    matrix<double> solucion(brows, bcols); solucion.null();
+    solucion(0,0) = -0.2568; solucion(1,0) = -1.0236;
+    solucion(2,0) = 0.4527; solucion(3,0) = 0.1014;
+
+    // Definimos la que será la matriz convergencia (resta entre nuestra solución y la exacta)
+    matrix<double> convergencia(brows, bcols); convergencia.null();
+
+    // Definimos la tolerancia y el valor que tomará la norma máxima de la matriz convergencia.
+    // Con la tolerancia más pequeña el programa tarda demasiado. Es por esto por lo que pongo 10^-4 de tolerancia.
+    double tol = 0.0001, normaMaxima = 0.0;
+
+    // Primera iteración
+    old_x = B;
+    iteraciones = 1;
+
+    // Resto de iteraciones
+    while (true){
+        new_x = B + (new_A*old_x);
+        old_x = new_x;
+        iteraciones++;
+        convergencia = solucion - new_x;
+        normaMaxima = NormaMaximaMatriz(convergencia);
+        // Si la norma máxima es menor que la tolerancia continuamos
+        if (normaMaxima<=tol){
+            break;
+        // Sino seguimos iterando.
+        } else {
+            continue;
+        }
+    }
+
+    // Por el método de Jacobi se hacen estas iteraciones.
+    cout<<"Método de Jacobi:"<<endl;
+    cout<<"Número de iteraciones: "<<iteraciones<<endl;
+
+    // Retornamos nuestra matriz solución.
+    return new_x;
+}
+
+matrix<double> MetodoGauss(matrix<double> A, matrix<double> B){
+    int iteraciones = 0;
+    int arows = 0, acols = 0, brows = 0, bcols = 0;
+    arows = A.rowno();
+    acols = A.colno();
+    brows = B.rowno();
+    bcols = B.colno();
+
+    matrix<double> old_x(brows, bcols); old_x.null();
+    matrix<double> new_x(brows, bcols); new_x.null();
+    matrix<double> new_A(arows, acols); new_A.null();
+
+    // Calculamos las matrices de coeficientes
+    for (int i=0; i<arows; i++){
+        B(i,0) = B(i,0)/A(i,i);
+        for(int j=0; j<acols; j++){
+            if(i!=j){
+                new_A(i,j) = -A(i,j)/A(i,i);
+            } else {
+                new_A(i,j) = 0;
+            }
+        }
+    }
+
+    // Definimos la matriz solución exacta
+    matrix<double> solucion(brows, bcols); solucion.null();
+    solucion(0,0) = -0.2568; solucion(1,0) = -1.0236;
+    solucion(2,0) = 0.4527; solucion(3,0) = 0.1014;
+
+    // Definimos la que será la matriz convergencia (resta entre nuestra solución y la exacta)
+    matrix<double> convergencia(brows, bcols); convergencia.null();
+
+    // Definimos la tolerancia y el valor que tomará la norma máxima de la matriz convergencia.
+    // Con la tolerancia más pequeña el programa tarda demasiado. Es por esto por lo que pongo 10^-4 de tolerancia.
+    double tol = 0.0001, normaMaxima = 0.0;
+
+    int i = 0;
+    while (true){
+        new_x(i,0) = B(i,0) + (new_A*old_x)(i,0);
+        old_x(i,0) = new_x(i,0);
+        if (i<4){
+            i++;
+        } else {
+            i = 0;
+            iteraciones++;
+            convergencia = solucion - new_x;
+            normaMaxima = NormaMaximaMatriz(convergencia);
+            if (normaMaxima<=tol){
+                break;
+            } else {
+                continue;
+            }
+        }
+    }
+
+    // Por el método de Gauss-Seidel se hacen estas iteraciones.
+    cout<<"Método de Gauss-Seidel:"<<endl;
+    cout<<"Número de iteraciones: "<<iteraciones<<endl;
+
+    return new_x;
 }
 
 int main(){
@@ -126,18 +234,16 @@ int main(){
                 cout<<"¡Cuidado! La matriz A no es diagonal dominante."<<endl;
             }
 
+            cout<<endl;
+            
             matrix<double> X(brows, bcols);
             X = MetodoJacobi(A, B);
-            cout<<X;
+            cout<<"Matriz solución tras ese número de iteraciones: "<<endl<<X;
+            cout<<endl;
 
-            /*double maximo = 0.0;
-            maximo = NormaMaximaMatriz(A);
-            cout<<maximo<<endl;
-
-            int len = 3;
-            double vector[len] = {-5, 2, 8};
-            maximo = NormaMaximaVector(vector, len);
-            cout<<maximo<<endl;*/
+            X = MetodoGauss(A, B);
+            cout<<"Matriz solución tras ese número de iteraciones: "<<endl<<X;
+            cout<<endl;
 
         }
     }
---
+++ PrácticaVIII/README.txt
@@ -0,0 +1,13 @@
+En esta carpeta .zip, llamada PracticaVIII, se encuentran los siguientes archivos:
+
+	1. EnunciadoPractica.pdf: Contiene el enunciado con lo que se pide en la práctica.
+	2. CodigoPracticaVIII.cpp: Contiene el código de la práctica en el lenguaje de programación C++.
+	En él se pueden encontrar las funciones de comprobación de si una matriz es 'dominante diagonal'
+	o no, las de cáclulo de la norma máxima pasando como argumento un vector o una matriz, las de 
+	cálculos de los métodos iterativos Jacobi y Gauss-Saidel y la función principal donde se llama a
+	las demás y se leen las matrices de archivos .txt externos.
+	3. InformePracticaVIII.PabloGradolphOliva.pdf: Contiene el informe de la práctica en el que se 
+	comentan los resultados obtenidos durante la misma en formato pdf.
+	4. matrizA.txt y matrizB.txt: Archivos externos donde se guardan los valores de las matrices, estos
+	valores pueden ser cambiados para la resolución de otro sistema y el programa funcionaría con normalidad.
+	
